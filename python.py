cd4417f2f73f261c1579feb687ec291374c26c45
BinuRai23 <r.binu024@gmail.com>
2024-06-26 15:12:27 +0545

    'day2'

---
+++ python.py
@@ -0,0 +1,39 @@
+dis= {'name':'binu',
+    'age' : 23}
+
+# print(dis.get('age'))
+# del dis ['age']
+# if 'binu' in dis:
+#     print('name exist')
+    
+# dis['age']=23
+# dis['isNepali']=True
+# print(dis.keys())
+# print(dis.values())
+# print(dis)
+
+# my_tuple=(1,2,3,5)
+# my_tuple.pop()
+
+# for list
+fruits= ['apple','mango']
+for i in fruits:
+    print(i)
+    
+# for tuple
+fruits= ('apple','mango')
+for i in fruits:
+    print(i)
+    
+# for set 
+fruits= {'apple','mango'}
+for i in fruits:
+    print(i)
+
+# for dictionary
+# for i in dis:
+#     print(dis[i])
+    
+message='this much for today'
+for char in message:
+    print(char)
